4b9c0c05459f69f6ad4b195bb529b2cb8069250e
Jochem Dobber <45901428+joggjogg@users.noreply.github.com>
2024-09-18 09:59:45 -0400

    Use Postmark api instead of nodejs client

--- util/email.ts
+++ util/email.ts
@@ -1,18 +1,21 @@
-var postmark = require('postmark')
 import EmailRequest from './types/emailRequest'
-
-const postmarkClient = new postmark.ServerClient(process.env.POSTMARK_KEY!)
 
 const sendEmail = async (data: EmailRequest) => {
   try {
-    const result = await postmarkClient.sendEmailWithTemplate({
-      TemplateId: 37250157,
-      From: 'mail@jochemdobber.nl',
-      To: 'mail@jochemdobber.nl',
-      TemplateModel: data,
-      MessageStream: 'outbound',
+    return await fetch('https://api.postmarkapp.com/email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Postmark-Server-Token': process.env.POSTMARK_KEY!,
+      },
+      body: JSON.stringify({
+        TemplateId: 37250157,
+        From: 'mail@jochemdobber.nl',
+        To: 'mail@jochemdobber.nl',
+        TemplateModel: data,
+        MessageStream: 'outbound',
+      }),
     })
-    console.debug(result)
   } catch (error: any) {
     console.debug(error)
     throw error
