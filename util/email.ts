e47c94595628bdd4e4be123c99aa9b3c6c63e1fa
Jochem Dobber <45901428+joggjogg@users.noreply.github.com>
2024-09-18 09:39:57 -0400

    Refactor email api

---
+++ util/email.ts
@@ -0,0 +1,22 @@
+import postmark from 'postmark'
+import EmailRequest from './types/emailRequest'
+
+const postmarkClient = new postmark.ServerClient(process.env.POSTMARK_KEY!)
+
+const sendEmail = async (data: EmailRequest) => {
+  try {
+    const result = await postmarkClient.sendEmailWithTemplate({
+      TemplateId: 37250157,
+      From: 'mail@jochemdobber.nl',
+      To: 'mail@jochemdobber.nl',
+      TemplateModel: data,
+      MessageStream: 'outbound',
+    })
+    console.debug(result)
+  } catch (error: any) {
+    console.debug(error)
+    throw error
+  }
+}
+
+export default sendEmail
